302ffd01d84a5f07692a10be8888baa30e686e39
art_terex <art133140@gmail.com>
2018-10-14 16:46:10 +0300

    Минифицировал файлы css и js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Плавный скролл к якорям
+
 $(document).ready(function(){
     $('.page-header a').click( function(){ // ловим клик по ссылке с классом go_to
   var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
@@ -7,3 +9,45 @@ $(document).ready(function(){
       return false; // выключаем стандартное действие
     });
 });
+
+// Слайдер в блоке Портфолио
+
+ // Индекс слайда по умолчанию 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+ // Функция увеличивает индекс на 1, показывает следующй слайд
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+ // Функция уменьшяет индекс на 1, показывает предыдущий слайд
+function minusSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+ // Устанавливает текущий слайд 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+ // Основная функция слайдера 
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
--- php/mail.php
+++ php/mail.php
@@ -1,10 +1,25 @@
 <?php
-$to = "art133140@gmail.com"; // емайл получателя данных из формы
-$tema = "Новый заказ"; // тема полученного емайла
-$message = "Ваше имя: ".$_POST['client-name']."<br>";//присвоить переменной значение, полученное из формы name=name
-$message .= "E-mail: ".$_POST['client-mail']."<br>"; //полученное из формы name=email
-$message .= "Номер телефона: ".$_POST['client-phone']."<br>"; //полученное из формы name=phone
-$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+if (!empty($_POST['client-name']) AND !empty($_POST['client-phone']) AND !empty($_POST['client-mail']))
+{
+    $headers = 'From: RubiWood
+' .
+                'Reply-To: art133140@gmail.com
+' .
+                'X-Mailer: PHP/' . phpversion();
+
+    $theme = "Новый заказ";
+    $letter = "Данные сообщения:";
+    $letter .="Имя: ".$_POST['client-name'];
+    $letter .="Email: ".$_POST['client-mail'];
+    $letter .="Телефон: ".$_POST['client-phone'];
+
+    if (mail("art133140@gmail.com", $theme, $letter, $headers)){
+      header("Location: /index.html");
+    } else {
+      header("Location: /index.html");
+    }
+
+} else {
+  header("Location: /index.html");
+}
 ?>
